247c0b17041edea4ded832c7ecc74f14f1b5da00
csetowhid <towhidhasang1@gmail.com>
2021-10-14 16:16:22 +0600

    design before excel

---
+++ resources/views/studentupload/_form.blade.php
@@ -0,0 +1,13 @@
+<div class="card-body">
+
+  <div class="input-group">
+    <div class="custom-file">
+      <input type="file" class="custom-file-input">
+      <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+    </div>
+    
+  </div>
+  </div>
+  <div class="card-footer">
+    <button type="submit" class="btn btn-primary">Upload</button>
+  </div>
--- resources/views/studentupload/create.blade.php
+++ resources/views/studentupload/create.blade.php
@@ -1,1 +1,46 @@
-<h1>asce</h1>
+@extends('home.layouts.app')
+@section('content')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Upload Student</h1>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="mx-auto col-lg-8 col-8">
+         
+            <div class="card card-primary">
+                <div class="card-header">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 class="card-title">Add New Student</h3>
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-end">
+                      <a href="{{route('students.index')}}">All Students</a>
+                    </div>
+                  </div>
+                  
+                </div>
+                <form action="{{route('students.store')}}" method="POST">
+                  @csrf
+                  @include('studentupload._form', ['buttonText' => __('Create')])
+                </form>
+              </div>
+        </div>
+
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+@endsection
