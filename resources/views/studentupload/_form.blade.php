af6858213876579309ca6a81051d65314b2ba35c
csetowhid <towhidhasang1@gmail.com>
2021-10-18 20:07:13 +0600

    marks upload start

--- resources/views/studentupload/_form.blade.php
+++ resources/views/studentupload/_form.blade.php
@@ -2,7 +2,7 @@
 
   <div class="input-group">
     <div class="custom-file">
-      <input type="file" class="custom-file-input" name="file">
+      {{-- <input type="file" class="custom-file-input" name="file"> --}}
 
       {{-- <input type="file" class="custom-file-input" name="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"> --}}
 
